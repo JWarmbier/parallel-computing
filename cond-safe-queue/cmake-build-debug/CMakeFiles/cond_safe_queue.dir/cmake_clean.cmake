84a9b11f794cc97848a1d193610e4758e517df83
JWarmbier <kerraiosc@gmail.com>
2019-05-02 21:49:51 +0200

    Complete safe definition of queue which provides safety in multi-thread applications

---
+++ cond-safe-queue/cmake-build-debug/CMakeFiles/cond_safe_queue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cond_safe_queue.dir/main.cpp.o"
+  "cond_safe_queue.pdb"
+  "cond_safe_queue"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cond_safe_queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
